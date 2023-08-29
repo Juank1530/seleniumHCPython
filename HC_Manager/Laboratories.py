36bd65f70815df0ab3cf89f3725343d86f0a8992
juank153021@gmail.com <juank153021@gmail.com>
2023-08-29 16:40:53 -0500

    Finished laboratories

--- HC_Manager/Laboratories.py
+++ HC_Manager/Laboratories.py
@@ -4,17 +4,27 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import time
 import config
 import pyautogui
 
+
+xpath_specialty = "/html/body/div/div/div/div[2]/div/div[2]/div/div[1]"
+patient_id = "id_paciente_presbicia"
+cord_x = 1025
+cord_y = 197
+btn_print_id = "btn-imprimir"
+xpath_btn_close = "/html/body/div[17]/div[1]/div/a[1]"
+table_id = '/html/body/div[1]/div[1]/section[2]/div[3]/div[3]/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/div'
+max_rows = '/html/body/div[1]/div[1]/section[2]/div[3]/div[3]/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div[1]/div/label/select'
 
 # Browser's driver setup
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 
 
-driver_service = Service(executable_path='C:\Drivers')
+driver_service = Service(executable_path='C:/Drivers/64/chromedriver.exe')
 driver = webdriver.Chrome(service=driver_service)
 
 # Browser's driver setup
@@ -36,11 +46,11 @@ time.sleep(3)
 
 # Select HC
 wait = WebDriverWait(driver, 60)
-hc = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div[2]/div/div[2]/div/div[1]")))
+hc = wait.until(EC.element_to_be_clickable((By.XPATH, xpath_specialty)))
 hc.click()
 
 # Looking for patient
-driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[1]/div/input").send_keys('999999')
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[1]/div/input").send_keys(config.id_patient[0])
 driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[3]/div/button").click()
 time.sleep(15)
 
@@ -48,55 +58,68 @@ time.sleep(15)
 fa = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/aside[1]/div/section/div[3]/ul/li[18]/a/span")))
 fa.click()
 
-
-# To send the new patient
-id = wait.until(EC.element_to_be_clickable((By.ID, "id_paciente_presbicia")))
-id.click()
-id.clear()
-id.send_keys(config.id_patient[0])
-
-#To change the focus for the new patient
-patient_name = driver.find_element(By.ID, "nombre_paciente_presbicia").send_keys(Keys.TAB)
-time.sleep(7)
-
 for list in range(len(config.id_patient)):
-    # Open HC
-    #driver.find_element(By.XPATH, "/html/body/div[1]/aside[1]/div/section/div[3]/ul/li[11]/a").click()
-    time.sleep(3)
 
-    # Select print button
-    wait = WebDriverWait(driver, 60)
-    btn_print = wait.until(EC.element_to_be_clickable((By.ID, "btn-imprimir")))
-    btn_print.click()
-
-    # Wait
-    time.sleep(7)
-
-    # Change the context to the iframe
-    #driver.switch_to.frame("iframe-impresion")
-
-    # Click on download button
-    pyautogui.click(x=1027, y=200) #Local
-    ##pyautogui.click(x=1278, y=254) #Server
-
-    # Wait for the files manager
+    # Select max rows
+    select_element = driver.find_element(By.XPATH, max_rows)
+    select = Select(select_element)
     time.sleep(8)
+    select.select_by_index(3)
+    time.sleep(5)
 
-    # Set a new name to the file
-    pyautogui.write(config.files_names[list])
+    time.sleep(6)
+    # Find table by id
+    table = driver.find_element(By.XPATH, table_id)  # Reemplaza con el selector de tu tabla
 
-    # To Press Enter to confirm and save the file
-    pyautogui.press("enter")
-    time.sleep(4)
+    # Find all rows into the table
+    rows = table.find_elements(By.TAG_NAME, 'tr')
 
-    # To close the pdf view
-    window = driver.window_handles
-    driver.switch_to.window(window[-1])
-    close = driver.find_element(By.XPATH, "/html/body/div[17]/div[1]/div/a[1]")
-    close.click()
+    # Count all rows
+    rows_quantity = len(rows)
+    print(rows_quantity)
+    time.sleep(2)
+
+    for n in range(1, rows_quantity):
+
+        driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/section[2]/div[3]/div[3]/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[2]/div/table/tbody/tr[' + str(n) + ']/td[1]/div').click()
+
+        # Open HC
+        #driver.find_element(By.XPATH, "/html/body/div[1]/aside[1]/div/section/div[3]/ul/li[11]/a").click()
+        time.sleep(3)
+
+        # Select print button
+        wait = WebDriverWait(driver, 60)
+        btn_print = wait.until(EC.element_to_be_clickable((By.ID, btn_print_id)))
+        btn_print.click()
+
+        # Wait
+        time.sleep(7)
+
+        # Change the context to the iframe
+        #driver.switch_to.frame("iframe-impresion")
+
+        # Click on download button
+        pyautogui.click(x = cord_x, y = cord_y) #Local
+        ##pyautogui.click(x=1278, y=254) #Server
+
+        # Wait for the files manager
+        time.sleep(8)
+
+        # Set a new name to the file
+        pyautogui.write(config.files_names[list] + '_' + str(n))
+
+        # To Press Enter to confirm and save the file
+        pyautogui.press("enter")
+        time.sleep(4)
+
+        # To close the pdf view
+        window = driver.window_handles
+        driver.switch_to.window(window[-1])
+        close = driver.find_element(By.XPATH, xpath_btn_close)
+        close.click()
 
     # To send the new patient
-    id = driver.find_element(By.ID, "id_paciente_presbicia")
+    id = driver.find_element(By.ID, patient_id)
     id.click()
     id.clear()
     id.send_keys(config.id_patient[list+1])
@@ -104,6 +127,5 @@ for list in range(len(config.id_patient)):
     #To change the focus for the new patient
     patient_name = driver.find_element(By.ID, "nombre_paciente_presbicia").send_keys(Keys.TAB)
     time.sleep(4)
-
 
 driver.quit()
---
+++ HC_Manager/NurseNotesMasive.py
@@ -0,0 +1,151 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import random
+import time
+import config
+import pyautogui
+
+
+xpath_specialty = "/html/body/div/div/div/div[2]/div/div[2]/div/div[1]"
+patient_id = "id_paciente_presbicia"
+cord_x = 1025
+cord_y = 197
+btn_print_id = "btn-imprimir"
+xpath_btn_close = "/html/body/div[22]/div[1]/div"
+table_id = '/html/body/div[1]/div[1]/section[2]/div[3]/div/div[2]/form/div/div[2]/div[4]/div[2]/table'
+max_rows = '/html/body/div[1]/div[1]/section[2]/div[3]/div/div[2]/form/div/div[2]/div[1]/label/select'
+
+# Browser's driver setup
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+
+driver_service = Service(executable_path='C:/Drivers/64/chromedriver.exe')
+driver = webdriver.Chrome(service=driver_service)
+
+# Browser's driver setup
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver.maximize_window()
+
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="LATEST").install()), options=options)
+
+# Opening URL
+driver.get(config.url)
+time.sleep(2)
+
+# Sing in
+driver.find_element(By.ID, "usuario").send_keys(config.username)
+driver.find_element(By.ID, "clave").send_keys(config.password)
+driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/form/input[2]").click()
+time.sleep(3)
+
+# Select HC
+wait = WebDriverWait(driver, 10)
+hc = wait.until(EC.element_to_be_clickable((By.XPATH, xpath_specialty)))
+hc.click()
+
+# Looking for patient
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[1]/div/input").send_keys(config.id_patient[0])
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div[3]/div/button").click()
+time.sleep(10)
+
+# Open HC
+driver.find_element(By.XPATH, "/html/body/div[1]/aside[1]/div/section/div[3]/ul/li[12]/a/span[1]").click()
+driver.find_element(By.XPATH, "/html/body/div[1]/aside[1]/div/section/div[3]/ul/li[12]/ul/li[1]/a").click()
+
+for list in range(len(config.id_patient)):
+
+    # Select max rows
+    select_element = driver.find_element(By.XPATH, max_rows)
+    select = Select(select_element)
+    time.sleep(8)
+    select.select_by_index(3)
+    time.sleep(5)
+
+    # To get total pages to me a loop
+    element = driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/section[2]/div[3]/div/div[2]/form/div/div[2]/div[5]")
+    text = element.text
+    total_pages = text.split()[9]
+    total_pages = int(int(total_pages) / 50) + 1
+
+    for page in range(1, total_pages):
+
+        time.sleep(6)
+        # Find table by id
+        table = driver.find_element(By.XPATH,table_id)  # Reemplaza con el selector de tu tabla
+
+        # Find all rows into the table
+        rows = table.find_elements(By.TAG_NAME,'tr')
+
+        # Count all rows
+        rows_quantity = len(rows)
+        time.sleep(2)
+
+        for n in range(1, rows_quantity):
+            
+            driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/section[2]/div[3]/div/div[2]/form/div/div[2]/div[4]/div[2]/table/tbody/tr[' + str(n) + ']/td[2]/div').click()
+
+            # Open HC
+            #driver.find_element(By.XPATH, "/html/body/div[1]/aside[1]/div/section/div[3]/ul/li[11]/a").click()
+            time.sleep(3)
+    
+            # Select print button
+            wait = WebDriverWait(driver, 30)
+            btn_print = wait.until(EC.element_to_be_clickable((By.ID, btn_print_id)))
+            btn_print.click()
+    
+            # Wait
+            time.sleep(15)
+    
+            # Click on download button
+            pyautogui.click(x = cord_x, y = cord_y)
+    
+            # Wait for the files manager
+            time.sleep(8)
+    
+            # Generate a code to set a new name to the file
+            characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+            random_strings = ""
+            for i in range(10):
+                random_character = random.choice(characters)
+                random_strings += random_character
+            pyautogui.write(config.files_names[list]+random_strings)
+    
+            # To Press Enter to confirm and save the file
+            pyautogui.press("enter")
+            time.sleep(4)
+    
+            # To close the pdf view
+            window = driver.window_handles
+            driver.switch_to.window(window[-1])
+            close = driver.find_element(By.XPATH, xpath_btn_close)
+            close.click()
+            time.sleep(3)
+
+            # Log file
+            path_absolute = "C:/Users/juan.tamara/Desktop/log.txt"
+            with open(path_absolute, "a") as file:
+                file.write(str(config.id_patient[list]) + '_Page_' + str(page) + '_Row_' + str(n) + "\n")
+
+        # Scroll to asure the page change and click in next page
+        driver.execute_script("window.scrollBy(0, 500);")
+        wait = WebDriverWait(driver, 20)
+        wait.until(EC.element_to_be_clickable((By.ID, 'div-controles_next'))).click()
+
+    # To send the new patient
+    id = driver.find_element(By.ID, patient_id)
+    id.click()
+    id.clear()
+    id.send_keys(config.id_patient[list+1])
+
+    #To change the focus for the new patient
+    id.send_keys(Keys.TAB)
+    time.sleep(4)
+
+driver.quit()
+
