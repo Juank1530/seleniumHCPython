c462745a4aa8150d0eb5556c442bf7ae80dec179
juank153021@gmail.com <juank153021@gmail.com>
2023-09-13 10:53:10 -0500

    Improve file TestMoveFiles. has been inclued a function to use differents year

--- HC_Manager/TestMoveFiles.py
+++ HC_Manager/TestMoveFiles.py
@@ -3,24 +3,28 @@ import glob
 
 months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 number = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-year = '2023'
-specialty = 'Formulación'
+year = ['2019','2020','2021','2022','2023']
+specialty_origin = 'TF'
+specialty_destiny = 'HC_Terapia_Física'
 
-for i in range(len(months)):
-    # Ruta del directorio de origen
-    ruta_directorio_origen = 'C:/Users/juan.tamara/Desktop/HC_manager/files/stage/formulation/'
 
-    # Patrón para buscar archivos que comienzan con "2020-03-"
-    patron_archivos = f'{year}-{number[i]}-*'
+for j in range(len(year)):
 
-    # Definir la nueva ruta de destino donde deseas mover los archivos
-    ruta_carpeta_destino = f'C:/Users/juan.tamara/Desktop/HC_manager/{year}/{number[i]}_{months[i]}/{specialty}/'
-    #ruta_carpeta_destino = f'C:/Users/juan.tamara/Desktop/HC_manager/files/stage/test_{number[i]}_{months[i]}/'
+    for i in range(len(months)):
+        # Origin Path folder
+        origin_path = f'C:/Users/juan.tamara/Desktop/HC_manager/files/stage/{specialty_origin}/'
 
-    # Usar glob para obtener una lista de archivos que coincidan con el patrón
-    archivos_coincidentes = glob.glob(ruta_directorio_origen + patron_archivos)
+        # Pattern to search for files starting with "2020-03-"
+        files_pattern = f'{year[j]}_{number[i]}_*'
 
-    # Mover cada archivo a la carpeta de destino
-    for archivo in archivos_coincidentes:
-        shutil.move(archivo, ruta_carpeta_destino)
+        # To Define the new destination path where you want to move the files
+        destiny_path = f'C:/Users/juan.tamara/Desktop/HC_manager/{year[j]}/{number[i]}_{months[i]}/{specialty_destiny}/'
+        #test_destiny_path = f'C:/Users/juan.tamara/Desktop/HC_manager/files/stage/{year[j]}/test/{number[i]}_{months[i]}/'
+
+        # Use glob to get a list of files that match the pattern
+        matching_files = glob.glob(origin_path + files_pattern)
+
+        # Move each file to the destination folder
+        for file in matching_files:
+            shutil.move(file, destiny_path)
 
